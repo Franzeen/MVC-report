c0d0e3658e51696e9077afc42d6efcb5d712340f
Adam Franzén <adam.fraanzen@gmail.com>
2023-04-17 21:30:56 +0200

    Removed unused method

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -13,20 +13,6 @@ class Card {
         $this->suit = null;
     }
 
-    public function draw(): array
-    {
-        $suits = array('hearts', 'diamonds', 'clovers', 'spades');
-        $values = array('ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king');
-        
-        $this->suit = $suits[array_rand($suits)];
-        $this->value = $values[array_rand($values)];
-
-        return array(
-            'value' => $this->value,
-            'suit' => $this->suit
-        );
-    }
-
     public function getValue(): string
     {
         return $this->value;
