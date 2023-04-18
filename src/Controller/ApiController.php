77e4029fa7e6dff429d84b506dd86ba608c1fa3c
Adam Franzén <adam.fraanzen@gmail.com>
2023-04-18 15:59:20 +0200

    Added routes for deck options

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Card\Card;
+use App\Card\CardGraphic;
+use App\Card\CardHand;
+use App\Card\DeckOfCards;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController
 {
     #[Route("/api/quote", name: "api_quote")]
-    public function quote(): Response
+    public function quote(): JsonResponse
     {
         $quotes = [
             "We are the masters of the unsaid words, but slaves of those we let slip out.",
@@ -29,5 +34,87 @@ class ApiController
         ];
 
         return new JsonResponse($data);
+    }
+
+    #[Route("api/deck", name: "api_deck")]
+    public function APIshowDeck(): JsonResponse
+    {
+        $deck = new DeckOfCards();
+        $deck->createDeck(new CardGraphic());
+
+        $data = [
+            'deck' => $deck->getAsString()
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("api/deck/shuffle", name: "api_deck_shuffle", methods: ['POST'])]
+    public function APIshuffleDeck(Request $request): JsonResponse
+    {
+        $session = $request->getSession();
+
+        $deck = new DeckOfCards();
+        $deck->createDeck(new CardGraphic());
+        $deck->shuffleDeck();
+
+        $session->set('deck', $deck);
+
+        $data = [
+            "shuffledDeck" => $deck->getAsString()
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("api/deck/draw/{num<\d+>}", name: "api_deck_draw", methods: ['POST'])]
+    public function APIdrawCard(Request $request, int $num = 1): JsonResponse
+    {
+        $session = $request->getSession();
+
+        if (!$session->has('deck')) {
+            $deck = new DeckOfCards();
+            $deck->createDeck(new CardGraphic());
+            $deck->shuffleDeck();
+            $session->set('deck', $deck);
+        } else {
+            $deck = $session->get('deck');
+        }
+
+        $drawCard = $deck->drawCard($num);
+
+        $cardHand = new CardHand();
+
+        foreach ($drawCard as $card) {
+            $cardHand->add($card);
+        }
+
+        $session->set('deck', $deck);
+
+        $remainingCards = $deck->getNumCards();
+
+        $drawCardStrings = [];
+        foreach ($drawCard as $card) {
+            $drawCardStrings[] = $card->getAsString();
+        }
+
+        $data = [
+            "drawCard" => $drawCardStrings,
+            "cardLeft" => $remainingCards,
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
     }
 }
